bce1855574db8cecec03a35549a2d4e97e6b7a53
Elena Grahovac <elena@grahovac.me>
2017-05-23 01:42:31 +0700

    Step 05. Add logger and tests on it

--- handlers.go
+++ handlers.go
@@ -10,3 +10,12 @@ import (
 func home(c *router.Control) {
 	fmt.Fprintf(c.Writer, "Processing URL %s...", c.Request.URL.Path)
 }
+
+// logger provides a log of requests
+func logger(c *router.Control) {
+	remoteAddr := c.Request.Header.Get("X-Forwarded-For")
+	if remoteAddr == "" {
+		remoteAddr = c.Request.RemoteAddr
+	}
+	log.Infof("%s %s %s", remoteAddr, c.Request.Method, c.Request.URL.Path)
+}
