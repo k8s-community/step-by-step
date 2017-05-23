002a254e44c13c081bb07a8f19318e98c6464603
Elena Grahovac <elena@grahovac.me>
2017-05-24 01:02:07 +0700

    Step 07. Add configuration

--- main.go
+++ main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"github.com/Sirupsen/logrus"
 	"github.com/takama/router"
+	"os"
 )
 
 var log = logrus.New()
 
-// Run server: go build && step-by-step
+// Run server: go build; env SERVICE_PORT=8000 step-by-step
 // Try requests: curl http://127.0.0.1:8000/test
 func main() {
+	port := os.Getenv("SERVICE_PORT")
+	if len(port) == 0 {
+		log.Fatal("Required parameter service port is not set")
+	}
+
 	r := router.New()
 	r.Logger = logger
 	r.GET("/", home)
-	r.Listen(":8000")
+	r.Listen("0.0.0.0:" + port)
 }
